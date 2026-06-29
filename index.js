17f000c4809c73eedfee6866a76670fcdfc639aa
Jose <trjdm@DESKTOP-ESRLAF0>
2026-06-29 17:09:18 -0500

    Cambios para la BD

--- index.js
+++ index.js
@@ -9,11 +9,8 @@ app.use(cors());
 app.use(express.json());
 
 const pool = new Pool({
-  user: process.env.DB_USER,
-  password: process.env.DB_PASSWORD,
-  host: process.env.DB_HOST,
-  port: process.env.DB_PORT,
-  database: process.env.DB_NAME,
+  connectionString: process.env.DATABASE_URL,
+  ssl: { rejectUnauthorized: false }
 });
 
 pool
--- init_db.js
+++ init_db.js
@@ -3,13 +3,8 @@ const { Pool } = require('pg');
 require('dotenv').config();
 
 const pool = new Pool({
-  user: process.env.DB_USER,
-  password: process.env.DB_PASSWORD,
-  host: process.env.DB_HOST,
-  port: process.env.DB_PORT,
-  database: process.env.DB_NAME,
-  // Esta línea es VITAL para cuando subamos a Render/Neon:
-  ssl: process.env.DATABASE_URL ? { rejectUnauthorized: false } : false
+  connectionString: process.env.DATABASE_URL,
+  ssl: { rejectUnauthorized: false }
 });
 
 const initScript = `
